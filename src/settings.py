276f7e46cb504c12d0b747436128a2ce3ecb8c40
Khaledmohamed0 <khaled.mohamed.tallat@gmail.com>
2026-07-03 06:55:37 +0300

    edit templates settings

--- src/settings.py
+++ src/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
